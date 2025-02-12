d16d081818d65ba004100c8b724809341d220a90
jayprakash2002 <143065634+jayprakash2002@users.noreply.github.com>
2025-02-12 22:06:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const express=require("express");
+const express =require("express");
 const app =express();
-const path= require("path");
+const path=require("path");
+
 
 const port =3030;
 
 app.set("view engine","ejs");
-app.set("views", path.join( __dirname,"/views"));
+app.set("views",path.join(__dirname,"/views"));
 
 app.get("/",(req, res)=>{
     res.render("home.ejs");
-}); 
-
-app.listen(port, ()=> {
-    console.log(`app is listening on port ${port}`);
 });
-
-
+app.get("/ig/:username",(req,res)=>{
+    let { username }= req.params;
+    const instaData=require("./data.json");
+    const data =instaData[username];
+    console.log(data);
+    res.render("instagram.ejs" ,{data });
+    //const followers=["adam","bob","stevn","abc"];
+    //let{username} =req.params;
+    //res.render("instagram.ejs",{ username,followers});
+});
+app.get("/hello",(req, res)=>{
+    res.send("hello");
+});
+app.get("/rolldice",(req, res)=>{
+    let diceVal =Math.floor(Math.random() *6) +1;
+    res.render("rolldice.ejs",{diceVal:diceVal });
+});
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+});
