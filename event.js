bab77f3a4f83e568c5d3cc02918a9b155470eb32
jayprakash2002 <143065634+jayprakash2002@users.noreply.github.com>
2025-02-17 23:53:05 +0530

    Add files via upload

---
+++ event.js
@@ -0,0 +1,14 @@
+//let inp=document.querySelector("input");
+
+
+//inp.addEventListener("keydown",function (event){
+    //console.log("key=", event.key);
+    //console.log("code=",event.code);
+    //console.log("key was pressed");
+
+//});
+let form =document.querySelector("form");
+
+form.addEventListener("submit",function (){
+    alert("form submitted");
+});
