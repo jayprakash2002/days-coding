bc27733c3db137f5994b437409e31c52eb03c1ef
jayprakash2002 <143065634+jayprakash2002@users.noreply.github.com>
2025-03-19 09:03:06 +0530

    Add files via upload

---
+++ pr.js
@@ -0,0 +1,40 @@
+//trim and uppercase 
+//let msg ="  hello  ";
+//let newmsg =msg.trim().toUpperCase();
+//console.log(newmsg);
+
+//let car =["audi","bmw","maruti"];
+
+///let month=["january","july","march","august"];
+//let monthone=["dec","nov","oct","sep"];
+//function Adult(){
+    ///let age =23;
+   /// if ( age>=18){
+       // console.log("you can vote");
+    //}
+    //else {
+        //console.log("you can not vote ");
+    ///}
+
+///}
+
+///Adult();
+
+///function names(name, age){
+    //console.log(`${name} 'is  age . ${age}`);
+
+//}  
+
+///names("jaoy",34);
+///function sum(a,b) {
+    //return a+b;
+//}
+
+//console.log(sum(sum(1,2),3));
+console.log("Hi there!");
+
+let id =setTimeout( ()=>{
+    console.log("aapna name")
+}, 4000 );
+
+console.log(id);
