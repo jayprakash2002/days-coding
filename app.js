248d68b860b2cbb15740f908d7a6dcf4b89cb150
jayprakash2002 <143065634+jayprakash2002@users.noreply.github.com>
2025-04-24 00:14:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,88 +1,133 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const path = require("path");
-//const methodOverride = require("method-override");
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-async function main() {
-  await mongoose.connect(MONGO_URL);
-}
-
-//app.set("view engine", "ejs");
-//app.set("views", path.join(__dirname, "views"));
-//app.use(express.urlencoded({ extended: true }));
-//app.use(methodOverride("_method"));
-
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
 });
+*/
 
-//Index Route
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-});
+/* Otherwise just put the config content (json): */
 
-//New Route
-//app.get("/listings/new", (req, res) => {
-  //res.render("listings/new.ejs");
-//});
-
-//Show Route
-//app.get("/listings/:id", async (req, res) => {
-  //let { id } = req.params;
-  //const listing = await Listing.findById(id);
-  //res.render("listings/show.ejs", { listing });
-//});
-
-//Create Route
-//app.post("/listings", async (req, res) => {
-  //const newListing = new Listing(req.body.listing);
-  //await newListing.save();
-  //res.redirect("/listings");
-//});
-
-//Edit Route
-///app.get("/listings/:id/edit", async (req, res) => {
-  //let { id } = req.params;
-  //const listing = await Listing.findById(id);
-  //res.render("listings/edit.ejs", { listing });
-//});
-
-//Update Route
-//app.put("/listings/:id", async (req, res) => {
-  //let { id } = req.params;
-  //await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  //res.redirect(`/listings/${id}`);
-//});
-
-//Delete Route
-///app.delete("/listings/:id", async (req, res) => {
-  //let { id } = req.params;
-  //let deletedListing = await Listing.findByIdAndDelete(id);
-  //console.log(deletedListing);
-  //res.redirect("/listings");
-//});
-
-//app.get("/testListing", async (req, res) => {
+particlesJS('particles-js',
   
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
 
-   //await sampleListing.save();
-   //console.log("sample was saved");
-   //res.send("successful testing");
- //});
-
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
-});
+);
